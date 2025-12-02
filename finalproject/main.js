d410f065d9073222e2e853ffebef3e4bd9ea74c4
Phoebe <plschropp@icloud.com>
2025-12-02 16:57:52 -0500

    update_12-2-25

--- finalproject/main.js
+++ finalproject/main.js
@@ -1,9 +1,227 @@
+// initialize the map:
+const map = L.map('map');
+map.setView([35.643445377615556, -82.55554263663021], 11);
+
+// add the tile layer to the map:
+const currentTileLayer = L.tileLayer(goodMap, {
+  attribution: '&copy; Stadia Maps contributors',
+}).addTo(map);
+
+// retrieve the landmarks from Google Sheets:
+// const detailsElement = document.querySelector('#details');
 async function getSMIE() {
-  const sheetName = 'SMIE Site Coordinates & Directions';
+  const sheetName = 'SMIE';
   const response = await fetch(
     `https://script.google.com/macros/s/AKfycbwjABaMDStuVqLz8WN6vUbbjXsMM1RKAmmPcdsN6zuwiytNLfmsfXbLeRLBZrE2n5zh/exec?sheet=${sheetName}`
   );
-  const SMIE = await response.json();
-  console.log('serverData: ', SMIE);
-  console.log('');
+  const sites = await response.json();
+  console.log('serverData: ', sites);
+  return sites;
 }
+
+async function generateMarkers() {
+  const sites = await getSMIE();
+
+  // Add markers for each landmark
+  for (const site of sites) {
+    // Create marker with popup
+    L.marker([site.LAT, site.LON], {
+      icon: getIcon(site.color, site.number),
+    })
+      .addTo(map)
+      .bindPopup(site.name)
+      .on('click', function (e) {
+        const detailsElement = document.querySelector('#details');
+        detailsElement.innerHTML = getTemplate(site);
+      });
+  }
+}
+
+function getTemplate(site) {
+  return `
+      <div>
+          <h2 style="margin: 0 0 5px 0;">${site.name}</h2>
+          <h4>Site Number: ${site.number}</h4>
+          <h3>Spring 2024 Rating: ${site.rating24}</h3>
+          <h3>Spring 2025 Rating: ${site.rating25}</h3>
+          <p>
+          ${site.LAT.toFixed(4)}, ${site.LON.toFixed(4)}
+          </p>
+      </div>
+      `;
+}
+
+function getIcon(color, number) {
+  return L.divIcon({
+    className: 'custom-div-icon',
+    html: `<div style="background-color: ${color}"
+      class="map-marker">${number}</div>`,
+    iconSize: [50, 50],
+    iconAnchor: [25, 25],
+  });
+}
+
+generateMarkers();
+
+
+
+// initialize the map:
+const map2 = L.map('map2');
+map2.setView([35.643445377615556, -82.55554263663021], 11);
+
+// add the tile layer to the map:
+const currentTileLayer2 = L.tileLayer(terrain, {
+  attribution: '&copy; Stadia Maps contributors',
+}).addTo(map2);
+
+async function generateMarkers2() {
+  const sites = await getSMIE();
+
+  // Add markers for each landmark
+  for (const site of sites) {
+    // Create marker with popup
+    L.marker([site.LAT, site.LON], {
+      icon: getIcon(site.color25, site.number),
+    })
+      .addTo(map2)
+      .bindPopup(site.name)
+      .on('click', function (e) {
+        const detailsElement2 = document.querySelector('#details2');
+        detailsElement2.innerHTML = getTemplate2(site);
+      });
+  }
+}
+
+function getTemplate2(site) {
+  return `
+      <div>
+          <h2 style="margin: 0 0 5px 0;">${site.name}</h2>
+          <h4>Site Number: ${site.number}</h4>
+          <h3>Spring 2024 Rating: ${site.rating24}</h3>
+          <h3>Spring 2025 Rating: ${site.rating25}</h3>
+          <p>
+          ${site.LAT.toFixed(4)}, ${site.LON.toFixed(4)}
+          </p>
+      </div>
+      `;
+}
+
+function getIcon2(color25, number) {
+  return L.divIcon({
+    className: 'custom-div-icon',
+    html: `<div style="background-color: ${color25}"
+      class="map-marker">${number}</div>`,
+    iconSize: [50, 50],
+    iconAnchor: [25, 25],
+  });
+}
+
+generateMarkers2();
+
+
+
+// define the funciton to create the chart:
+async function createChart() {
+  const totalData = await getSMIE();
+
+  // create the two lists that we need for the chart:
+  spring24 = totalData.map(row => row['totals spring 2024']);
+  spring25 = totalData.map(row => row['totals spring 2025']);
+  siteNames = totalData.map(row => row['name']);
+  siteNumber = totalData.map(row => row['number']);
+
+  const options = {
+    chart: {
+      type: 'bar',
+      events: {
+        dataPointSelection: getDetails,
+      },
+    },
+    colors: ['#2c7bb6', '#d7191c'], // series 1 and series 2 colors
+    plotOptions: {
+      bar: {
+        horizontal: true,
+      },
+    },
+    series: [
+      { name: 'Total number of individuals spring 2024', data: spring24 },
+      { name: 'Total number of individuals spring 2025', data: spring25 },
+    ],
+    xaxis: { categories: siteNames },
+  };
+
+  document.querySelector('#chart').innerHTML = '';
+  const chart = new ApexCharts(document.querySelector('#chart'), options);
+  chart.render();
+}
+
+// Much simpler function using our stored data:
+function getDetails(event, chartContext, config) {
+  const dataIndex = config.dataPointIndex; // get the index of the clicked point
+
+  // target the details panel:
+  const detailsElement = document.querySelector('#chartDetails');
+  // update the details panel:
+  detailsElement.innerHTML = `
+          <h2>Details for ${siteNames[dataIndex]}</h2>
+          <p>Site Number: ${siteNumber[dataIndex]}</p>
+          <p>Total Number of Individuals Spring 2024: ${spring24[dataIndex]}</p>
+          <p>Total Number of Individuals Spring 2025: ${spring25[dataIndex]}</p>
+      `;
+}
+
+// run the function:
+createChart();
+
+
+
+// define the funciton to create the chart:
+async function createLineChart() {
+  const totalData = await getSMIE();
+
+  // create the two lists that we need for the chart:
+  difference = totalData.map(row => row['difference']);
+  siteNames = totalData.map(row => row['name']);
+  siteNumber = totalData.map(row => row['number']);
+
+  const options = {
+    chart: {
+      type: 'bar',
+      events: {
+        dataPointSelection: getDetails2,
+      },
+    },
+    plotOptions: {
+      bar: {
+        horizontal: true,
+      },
+    },
+    colors: ['#2c7bb6'], // series 1 and series 2 colors
+    
+    series: [
+      { name: 'Difference from before Helene to after', data: difference },
+    ],
+    xaxis: { categories: siteNames },
+  };
+
+  document.querySelector('#chart2').innerHTML = '';
+  const chart = new ApexCharts(document.querySelector('#chart2'), options);
+  chart.render();
+}
+
+// Much simpler function using our stored data:
+function getDetails2(event, chartContext, config) {
+  const dataIndex = config.dataPointIndex; // get the index of the clicked point
+
+  // target the details panel:
+  const detailsElement2 = document.querySelector('#chartDetails2');
+  // update the details panel:
+  detailsElement2.innerHTML = `
+          <h2>Details for ${siteNames[dataIndex]}</h2>
+          <p>Site Number: ${siteNumber[dataIndex]}</p>
+          <p>Difference in Individuals After Helene: ${difference[dataIndex]}</p>
+      `;
+}
+
+// run the function:
+createLineChart();
